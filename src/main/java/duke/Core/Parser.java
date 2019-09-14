9cb3fbfbd5a27b3503c1cd18f0d4111cef91fdf3
MC <dandford@gmail.com>
2019-09-14 18:14:52 +0800

    Add update feature and minor refactoring

--- src/main/java/duke/Core/Parser.java
+++ src/main/java/duke/Core/Parser.java
@@ -7,17 +7,17 @@ import duke.command.DoneCommand;
 import duke.command.ExitCommand;
 import duke.command.ListCommand;
 import duke.command.FindCommand;
+import duke.command.UpdateCommand;
 import duke.helper.DukeException;
 
 public class Parser {
 
     /**
      * Reads and Understands the user's input command from class Ui.
-     * public method called by Duke.run() in the for loop as part of the Command Pattern.
      *
-     * @param inputCommand String that is obtained from the scanner which is located in Ui.
-     * @return Command which can be any one of the commands based on the first word of the user input parsed.
-     * @throws DukeException which happens if the user input is not one of the predefined inputs.
+     * @param inputCommand String which is the user input.
+     * @return Command any of the commands based on the first word of the user input parsed.
+     * @throws DukeException if the user input is not one of the predefined inputs.
      */
     public static Command parse(String inputCommand) throws DukeException {
         String[] inputsplit = inputCommand.split(" ", 2);
@@ -25,20 +25,29 @@ public class Parser {
         switch (parsedString) {
         case "list" :
             return new ListCommand(inputCommand);
+            break;
         case "done" :
             return new DoneCommand(inputCommand);
+            break;
         case "todo" :
         case "deadline":
         case "event":
             return new AddCommand(inputCommand);
+            break;
         case "delete":
             return new DeleteCommand(inputCommand);
+            break;
         case "find":
             return new FindCommand(inputCommand);
+            break;
+        case "update":
+            return new UpdateCommand(inputCommand);
         case "bye":
             return new ExitCommand(inputCommand);
+            break;
         default:
             throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+            break;
         }
     }
 }
